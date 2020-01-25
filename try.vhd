fa74635eaff290101dd5bd0f75e52a8da1464214
clashrover <sagar99x@gmail.com>
2020-01-25 18:07:19 +0530

    backup

--- try.vhd
+++ try.vhd
@@ -22,9 +22,10 @@ signal reg_file : reg_type :=("00000000000000000000000000000001",
                               "00000000000000000000000000000000"
                 );
 
--- Data Memory
---type mem is array(0 to 3999) of std_logic_vector(31 downto 0);
---signal memory : mem;
+Data Memory
+--type mem is array(0 to 2) of std_logic_vector(31 downto 0);
+--signal memory : mem :=(
+						);
 
 signal currentIns : std_logic_vector(31 downto 0) := "00000000000000010001000000010000";
 
@@ -32,51 +33,41 @@ begin
 	--currentIns <= memory(pc);
 	process(clk)
 	begin
-		if currentIns(31 downto 26)="000000" then 		-- check for addition
-			if clk='1' and clk'event then
-				if currentIns(5 downto 0)="010000" then
-					reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= std_logic_vector(unsigned(reg_file(to_integer(unsigned(currentIns(25 downto 21))))) + unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))));
+		if clk='1' and clk'event then
+
+			if currentIns(31 downto 26)="000000" then 		-- check for addition
+					if currentIns(5 downto 0)="100000" then
+						reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= std_logic_vector(unsigned(reg_file(to_integer(unsigned(currentIns(25 downto 21))))) + unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))));
+					end if;
+			end if;
+
+			if currentIns(31 downto 26)="000000" then 		-- check for subtraction
+				if currentIns(5 downto 0)="100010" then
+					reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= std_logic_vector(unsigned(reg_file(to_integer(unsigned(currentIns(25 downto 21))))) - unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))));
 				end if;
 			end if;
+
+			if currentIns(31 downto 26)="000000" then 		-- check for sll
+				if currentIns(5 downto 0)="000000" then 	--check func
+					reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= shift_left(unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))), to_integer(reg_file(to_integer(unsigned(currentIns(10 downto 6))))));
+				end if;
+			end if;
+
+			if currentIns(31 downto 26)="000000" then 		-- check for slr
+				if currentIns(5 downto 0)="000010" then 	--check func
+					reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= shift_right(unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))), to_integer(reg_file(to_integer(unsigned(currentIns(10 downto 6))))));
+				end if;
+			end if;
+
+	--		if currentInst(31 downto 26)="100011" then 		-- check for lw
+	--			reg_file(to_integer(unsigned(currentIns(20 downto 16)))) <= memory(to_integer(reg_file(to_integer(unsigned(currentIns(25 downto 21)))))+to_integer(unsigned(currentIns(15 downto 0))));
+	--		end if;
+
+	--		if currentInst(31 downto 26)="101011" then 		-- check for sw
+	--			memory(to_integer(reg_file(to_integer(unsigned(currentIns(25 downto 21)))))+to_integer(unsigned(currentIns(15 downto 0)))) <= reg_file(to_integer(unsigned(currentIns(20 downto 16))));
+	--		end if;
+	
 		end if;
-
---		if currentIns(31 downto 26)="000000" then 		-- check for subtraction
---			if clk='1' and clk'event then
---				if currentIns(5 downto 0)="----" then
---					reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= std_logic_vector(unsigned(reg_file(to_integer(unsigned(currentIns(25 downto 21))))) - unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))));
---				end if;
---			end if;
---		end if;
-
---		if currentIns(31 downto 26)="000000" then 		-- check for sll
---			if clk='1' and clk'event then
---				if currentIns(5 downto 0)="----" then 	--check func
---					reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= shift_left(unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))), to_integer(reg_file(to_integer(unsigned(currentIns(10 downto 6))))));
---				end if;
---			end if;
---		end if;
-
---		if currentIns(31 downto 26)="000000" then 		-- check for slr
---			if clk='1' and clk'event then
---				if currentIns(5 downto 0)="----" then 	--check func
---					reg_file(to_integer(unsigned(currentIns(15 downto 11)))) <= shift_right(unsigned(reg_file(to_integer(unsigned(currentIns(20 downto 16))))), to_integer(reg_file(to_integer(unsigned(currentIns(10 downto 6))))));
---				end if;
---			end if;
---		end if;
-
---		if currentInst(31 downto 26)="000000" then 		-- check for lw
---			if clk='1' and clk'event then
---					reg_file(to_integer(unsigned(currentIns(20 downto 16)))) <= Memory(to_integer(unsigned(currentIns(15 downto 0))));
---				end if;
---			end if;
---		end if;
-
---		if currentInst(31 downto 26)="000000" then 		-- check for sw
---			if clk='1' and clk'event then
---					Memory(to_integer(unsigned(currentIns(15 downto 0)))) <= reg_file(to_integer(unsigned(currentIns(20 downto 16))));
---				end if;
---			end if;
---		end if;
 	end process;
 end Behavioral;
 
